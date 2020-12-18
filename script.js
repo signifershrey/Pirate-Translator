9d790d15dac484bcb3682f3e356dabac7e407ec7
SHREY MAURYA <56041735+signifershrey@users.noreply.github.com>
2020-12-18 23:16:10 +0530

    docs- L6

---
+++ script.js
@@ -0,0 +1,28 @@
+var translateBtn = document.querySelector("#translate-btn");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with server! Try again after some time")
+}
+
+translateBtn.addEventListener("click", function clickEventHandler(){
+
+    var inputText =txtInput.value; //taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText
+    })
+    .catch(errorHandler)
+})
